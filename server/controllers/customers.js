7885b90e9d243f06cf339ad27ef007d682c41e9b
Michael Tran <miiketran@gmail.com>
2016-01-09 13:32:13 -0800

    fixed date string

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -17,7 +17,7 @@ module.exports = {
     var mydate = new Date(); 
     var month = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"][mydate.getMonth()];
-    var date_str = month + ' ' + mydate.getDate()+ " "+ mydate.getFullYear();
+    var date_str = month + ' ' + mydate.getDate()+ ", "+ mydate.getFullYear();
 
 
     console.log("adding");
